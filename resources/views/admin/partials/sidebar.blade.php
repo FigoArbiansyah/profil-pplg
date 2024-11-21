23e26c9c3dd19a6ba4086cea33ea8b2586423bdf
FigoArbiansyah <figoarbnsyh@gmail.com>
2024-11-21 10:56:11 +0700

    update icon sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -23,73 +23,73 @@
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/data-sekolah') ? 'active' : '' }}">
             <a href="/admin-pplg/data-sekolah" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-building"></i>
                 <div data-i18n="Analytics">Data Sekolah</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/data-jurusan') ? 'active' : '' }}">
             <a href="/admin-pplg/data-jurusan" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-book-open"></i>
                 <div data-i18n="Analytics">Data Jurusan</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/data-alumni') ? 'active' : '' }}">
             <a href="/admin-pplg/data-alumni" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-user"></i>
                 <div data-i18n="Analytics">Data Alumni</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/jumbotron') ? 'active' : '' }}">
             <a href="/admin-pplg/jumbotron" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-image"></i>
                 <div data-i18n="Analytics">Jumbotron</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/about') ? 'active' : '' }}">
             <a href="/admin-pplg/about" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-info-circle"></i>
                 <div data-i18n="Analytics">About</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/category') ? 'active' : '' }}">
             <a href="/admin-pplg/category" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-category"></i>
                 <div data-i18n="Analytics">Kategori</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/article') ? 'active' : '' }}">
             <a href="/admin-pplg/article" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-file-blank"></i>
                 <div data-i18n="Analytics">Artikel</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/keahlian') ? 'active' : '' }}">
             <a href="/admin-pplg/keahlian" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-brush"></i>
                 <div data-i18n="Analytics">Keahlian</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/teacher') ? 'active' : '' }}">
             <a href="/admin-pplg/teacher" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-user-voice"></i>
                 <div data-i18n="Analytics">Pengajar</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/mapel') ? 'active' : '' }}">
             <a href="/admin-pplg/mapel" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-book-open"></i>
                 <div data-i18n="Analytics">Mata Pelajaran</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/software') ? 'active' : '' }}">
             <a href="/admin-pplg/software" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-laptop"></i>
                 <div data-i18n="Analytics">Software</div>
             </a>
         </li>
         <li class="menu-item {{ request()->is('admin-pplg/logout') ? 'active' : '' }}">
             <a href="/logout" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <i class="menu-icon tf-icons bx bx-log-out"></i>
                 <div data-i18n="Analytics">Logout</div>
             </a>
         </li>
